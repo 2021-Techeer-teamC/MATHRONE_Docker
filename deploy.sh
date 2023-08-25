db85d435a5688f4276fbc28ff378c863841cf167
hyunwoo <ashyonu@gmail.com>
2023-08-25 23:37:02 +0900

    Update frontend: 20230825

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@ sudo docker-compose down --volumes
 sudo docker system prune -a -f
 cd /home/ec2-user/app/MATHRONE_Frontend_Submodule
 npm install
+cd /home/ec2-user/app/MATHRONE_Backend_Submodule
+sudo ./gradlew build
 cd ..
 sudo docker-compose up --build
